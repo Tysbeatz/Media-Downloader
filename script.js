678073f32fde2a74addd60530b4257d1a723798f
tysbeatz <99852682+Tysbeatz@users.noreply.github.com>
2026-03-09 12:44:27 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,14 @@
-async function download(){
+function download(){
 
 let url = document.getElementById("url").value
 
 if(url === ""){
-alert("Paste a link first")
+alert("Paste link first")
 return
 }
 
-let result = document.getElementById("result")
+let downloader = "https://en.savefrom.net/1-youtube-video-downloader-16/?url=" + encodeURIComponent(url)
 
-result.innerHTML = `
-<p>Click the button below to download</p>
-<a href="https://en.savefrom.net/1-youtube-video-downloader-16/?url=${url}" target="_blank">
-<button>Open Downloader</button>
-</a>
-`
+window.open(downloader,"_blank")
 
 }
